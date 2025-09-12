ee89a332984f3e68aebfc8cfa5a12ab2a81468e0
DIPAKK2310 <dipakkhare217@gmail.com>
2025-09-12 15:50:37 +0000

    feat(eslint)add for linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,24 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser },rules: {// allow empty catch blocks
+      "no-empty": ["warn", { allowEmptyCatch: true }],} },
+  tseslint.configs.recommended,
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    // React recommended config + React version detection
+    ...pluginReact.configs.flat.recommended,
+    settings: { react: { version: "detect" } },
+  },    {
+    ignores: ["node_modules/**", ".next/**", "generated/**", "prisma/generated/**"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "no-undef": "off",
+      "react/react-in-jsx-scope": "off", // Next.js 13+ app dir
+    },
   },
-];
-
-export default eslintConfig;
+]);
