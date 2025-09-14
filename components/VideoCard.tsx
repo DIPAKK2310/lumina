31a6b49e66f4af2f368036ce4ccc0608e830e5c0
DIPAKK2310 <dipakkhare217@gmail.com>
2025-09-14 10:36:56 +0000

    fix(ui)videoCard

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react"
 import { getCldImageUrl, getCldVideoUrl} from "next-cloudinary"
 import { Download, Clock, FileDown, FileUp} from "lucide-react"
-import dayjs from "dayjs"
+import dayjs from "dayjs"// import utc from "dayjs/plugin/utc"
 import relativeTime from "dayjs/plugin/relativeTime"// import localizedFormat from "dayjs/plugin/localizedFormat"
 import {filesize} from "filesize"
 import { Video } from "@prisma/client"
@@ -16,7 +16,29 @@ interface VideoCardProps {
 
 const VideoCard: React.FC<VideoCardProps> = ({video, onDownload}) => {
 
-    // 203)10:10
+  const [isHovered, setisHovered] = useState(false)
+  const [previewError, setpreviewError] = useState(false)
+
+  const getThumbnailUrl = useCallback((publicId: string) => {
+    return getCldImageUrl( {
+      src: publicId,
+      width: 400,
+      height: 225,
+      crop: "fill",
+      gravity: "auto",
+      format: "jpg",
+      quality: "auto",
+      assetType: "video"
+    })
+  },[])
+  const getFullVideolUrl = useCallback((publicId: string) => {
+    return getCldVideoUrl( {
+      src: publicId,
+      width: 1920,
+      height: 1080,
+     
+    })
+  },[])
   return (
     <div>VideoCard</div>
   )
