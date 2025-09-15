6aa29464944a36af0367182e291ba01d31e5d67b
DIPAKK2310 <dipakkhare217@gmail.com>
2025-09-15 07:50:10 +0000

    fix(clerkprovider) hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,9 +30,9 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
           >
             <ClerkProvider>
-          {children}
-          <Toaster position="top-right" reverseOrder={false}/>
-    </ClerkProvider>
+                {children}
+                <Toaster position="top-right" reverseOrder={false}/>
+            </ClerkProvider>
         </body>
       </html>
   );
