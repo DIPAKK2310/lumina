b7656cc38241f5cbaf526e0a27dc4722cab61a54
DIPAKK2310 <dipakkhare217@gmail.com>
2025-11-07 15:53:58 +0000

    Feat(types):Add

--- types/index.ts
+++ types/index.ts
@@ -1,10 +1,12 @@
-export interface Video {
+export default interface Video {
   id: string;
   title: string;
   description: string;
   publicId: string;
+  url: string;
   originalSize: number;
   compressedSize: number;
   duration: number;
   createdAt: Date;
+  updatedAt: Date;
 }
