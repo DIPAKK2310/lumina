57a4e7deb4928f8d5e13cd139980e8278616b5f7
DIPAKK2310 <dipakkhare217@gmail.com>
2025-11-24 09:02:21 +0000

    Feat(FAQ):Add

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Pricing from '@/components/Ui/Pricing';
 import CTA from '@/components/Ui/CTA';
 import AppContainer from '@/components/Ui/Background_Gradient';
 import Navbar from '@/components/Ui/Navbar';
+import PricingFAQ from '@/components/Ui/FAQ';
 
 export default function Page() {
 
@@ -22,6 +23,7 @@ export default function Page() {
       {/* Pricing Section */}
         <Pricing/>
       {/* CTA Section */}
+      <PricingFAQ/>
         <CTA/>
       {/* Footer */}
       <Footer/>
