23887ff3a0c584f681452645b2893ddfa98456c6
DIPAKK2310 <dipakkhare217@gmail.com>
2025-11-25 12:08:56 +0000

    Feat(FAQ):Add

--- components/Ui/FAQ.tsx
+++ components/Ui/FAQ.tsx
@@ -2,7 +2,7 @@ import { faqs } from "@/components/config/FAQ";
 
 export default function PricingFAQ() {
 return (
-    <section className="py-20 px-6 max-w-4xl mx-auto">
+    <section className="py-20 px-6 max-w-4xl mx-auto border-1 border-dotted rounded-xl mt-20">
         <h2 className="text-3xl font-bold text-center mb-10 bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-indigo-500">
             Frequently Asked Questions
         </h2>
